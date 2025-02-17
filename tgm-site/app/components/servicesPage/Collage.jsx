2212e9c885f2829d04d7ae5f2e131dc9ba2c6ca7
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2025-02-17 14:31:48 -0500

    services page mvp complete

---
+++ tgm-site/app/components/servicesPage/Collage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+import collagePicOne from "../../../public/images/collageOne.jpg";
+import collagePicTwo from "../../../public/images/collageTwo.jpeg";
+import collagePicThree from "../../../public/images/collageThree.jpg";
+import collagePicFour from "../../../public/images/collageFour.jpeg";
+import CollageComponent from "./CollageComponent";
+
+const Collage = () => {
+  return (
+    <div className="w-full h-auto flex justify-center ">
+      <div className="grid grid-cols-2 gap-3 md:gap-4 lg:gap-5 my-7">
+        <CollageComponent pic={collagePicOne} alt={"Mand holding grass"} className={"rounded-tl-none"}/>
+        <CollageComponent pic={collagePicThree} alt={"Man Cutting Shrubs"} className={"rounded-tr-none"}/>
+        <CollageComponent pic={collagePicTwo} alt={"Lawn Mower"} className={"rounded-bl-none"}/>
+        <CollageComponent pic={collagePicFour} alt={"Picture of a yard"} className={"rounded-br-none"}/>
+      </div>
+    </div>
+  );
+};
+
+export default Collage;
+
+// Above, we use our CollageComponent to render four images within a flex box
---
+++ tgm-site/app/components/servicesPage/ServiceCard.jsx
@@ -0,0 +1,92 @@
+"use client";
+import React, { useState } from "react";
+import "boxicons/css/boxicons.min.css";
+import Image from "next/image";
+
+const ServiceCard = ({ pic, title, serviceBio, plansDiscription, plans }) => {
+  const [showFirstElement, setShowFirstElement] = useState(false);
+
+  return (
+    <div
+      onClick={() => setShowFirstElement(!showFirstElement)}
+      className=" w-full sm:w-[75%] md:w-[40%] lg:w-[30%] h-auto mt-5"
+    >
+      <div className="w-full h-auto p-3 bg-darkGreen border border-gray-300 rounded-lg shadow-md hover:shadow-glass-sm space-y-5">
+        <div className="w-full h-[150px] relative">
+          <Image
+            src={pic}
+            alt="man cutting grass"
+            fill
+            objectFit="cover"
+            quality={100}
+            style={{ filter: "brightness(100%)" }}
+            className="rounded-lg"
+          />
+        </div>
+        <div className="space-y-2">
+          <div className="mt-5 text-2xl text-white font-bold">
+            {" "}
+            <h3>{title}</h3>
+          </div>
+          <div className="w-full h-auto  text-white text-md">
+            <p
+              className={` overflow-hidden ${
+                showFirstElement ? " " : "line-clamp-4"
+              }`}
+            >
+              {serviceBio}
+            </p>
+          </div>
+        </div>
+
+        {showFirstElement && (
+          <div className="w-full h-auto">
+            <div className="text-xl text-white font-bold">
+              {" "}
+              <h3>Plans:</h3>
+            </div>
+            <div className="mt-2 w-full h-auto text-white text-md">
+              <p>{plansDiscription}</p>
+            </div>
+            <div className="mt-4 w-full h-auto text-white text-md space-y-4">
+              {plans.map((plan, index) => {
+                return <p key={index}>{plan}</p>;
+              })}
+            </div>
+          </div>
+        )}
+
+        <div className=" p-2 relative w-full h-auto">
+          <div className="flex justify-start w-[50%]">
+            <button className="bg-emeraldOp text-white rounded border  border-white hover:text-emerald hover:bg-white p-1 text-[15px] md:text-sm ">
+              GET QUOTE
+            </button>
+          </div>
+
+          <div
+            className={`absolute bottom-1 right-1 text-white cursor-pointer ${
+              !showFirstElement ? "opacity-100" : "opacity-0"
+            } `}
+          >
+            <i className="bx bx-chevron-up text-3xl"></i>
+          </div>
+          {/* conditional rendering up or down arrow */}
+          <div
+            className={`absolute bottom-1 right-1 text-white cursor-pointer ${
+              showFirstElement ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <i className="bx bx-chevron-down text-3xl"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
+
+/* This is a responsive card component that accepts pic, title, serviceBio, plansDescription, and plans as props. 
+  The card is clickable and expands when selected. 
+  This functionality is achieved through conditional rendering of a div based on the value of showFirstElement.
+ */
